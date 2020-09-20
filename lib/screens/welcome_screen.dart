600fa3e0b0a3275a6008b90ecb87a57d16b5d5a2
shivam5550 <sharma.shivamhr@gmail.com>
2020-09-20 08:50:23 +0530

    first commit (welcome screen and register)

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cargoshuttle/constants.dart';
 import 'package:cargoshuttle/components/rounded_button.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class WelcomeScreen extends StatelessWidget {
   static const String id = 'welcome_screen';
@@ -23,18 +24,19 @@ class WelcomeScreen extends StatelessWidget {
               image: AssetImage('images/Truck Logo black.png'),
             ),
             RoundButton(
-              text: "Login",
+              text: "REGISTER",
               onPressed: () {
-                Navigator.pushNamed(context, LoginScreen.id);
+                Navigator.pushNamed(context, RegistrationHomeScreen.id);
               },
               color: themeColor,
             ),
             RoundButton(
-              text: "Register",
+              text: "LOGIN",
               onPressed: () {
-                Navigator.pushNamed(context, RegistrationHomeScreen.id);
+                Navigator.pushNamed(context, LoginScreen.id);
               },
               color: Colors.white,
+
             ),
           ],
         ),
